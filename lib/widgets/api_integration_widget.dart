darshmashru/ChatGemini

commit b92e43bd98bc895af30a975d1da862afc07fe7c7

Feat: Integration Widget Improvements (#14)
* Added type cursor

* Added an autoscroller

* Added stop button and removed Safety

* Fixed Project Warnings

* Update api_integration_widget.dart

---------

Co-authored-by: Darsh Mashru <mashrudarsh31@gmail.com>

lib/widgets/api_integration_widget.dart
@@ -1,35 +1,40 @@
-import 'dart:developer';
-import 'dart:typed_data';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 
-class ApiIntegrationWidget extends ConsumerStatefulWidget {
-  const ApiIntegrationWidget({Key? key}) : super(key: key);
+class ApiIntegrationWidget extends StatefulWidget {
+  const ApiIntegrationWidget({super.key});
 
   @override
   _ApiIntegrationWidgetState createState() => _ApiIntegrationWidgetState();
 }
 
-class _ApiIntegrationWidgetState extends ConsumerState<ApiIntegrationWidget>
+class _ApiIntegrationWidgetState extends State<ApiIntegrationWidget>
     with AutomaticKeepAliveClientMixin {
   final TextEditingController _promptInputController = TextEditingController();
   final ScrollController _outputScrollController = ScrollController();
   String mdText = "";
   final ImagePicker _picker = ImagePicker();
-  Uint8List? _imageBytes; // Use Uint8List for image bytes
+  Uint8List? _imageBytes;
   String? _errorMessage;
-  bool _isLoading = false; // Add this variable to track loading state
+  bool _isLoading = false;
+  late bool _isAsking;
+
+  @override
+  void initState() {
+    super.initState();
+    _isAsking = false;
+  }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
     return Padding(
-      padding: const EdgeInsets.all(8.0), // Adjust padding as needed
+      padding: const EdgeInsets.all(8.0),
       child: Scaffold(
         backgroundColor: Theme.of(context).colorScheme.background,
         body: Column(
@@ -45,9 +50,8 @@ class _ApiIntegrationWidgetState extends ConsumerState<ApiIntegrationWidget>
                         padding: const EdgeInsets.all(8.0),
                         child: ConstrainedBox(
                           constraints: BoxConstraints(
-                            maxWidth: MediaQuery.of(context).size.width -
-                                16, // Adjust the width as needed
-                            maxHeight: 300, // Adjust the height as needed
+                            maxWidth: MediaQuery.of(context).size.width - 16,
+                            maxHeight: 300,
                           ),
                           child: Image.memory(_imageBytes!, fit: BoxFit.cover),
                         ),
@@ -57,11 +61,11 @@ class _ApiIntegrationWidgetState extends ConsumerState<ApiIntegrationWidget>
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
                           _errorMessage!,
-                          style: TextStyle(color: Colors.red),
+                          style: const TextStyle(color: Colors.red),
                         ),
                       ),
                     MarkdownBody(
-                      data: mdText,
+                      data: _isLoading ? mdText : mdText,
                       selectable: true,
                       onTapLink: (text, href, title) {
                         if (href != null) {
@@ -102,6 +106,17 @@ class _ApiIntegrationWidgetState extends ConsumerState<ApiIntegrationWidget>
                     onPressed: _isLoading ? null : _copyText,
                     tooltip: 'Copy Text',
                     child: const Icon(Icons.copy),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FloatingActionButton(
+                    onPressed:
+                        _isAsking ? _stopAsking : (_isLoading ? null : _ask),
+                    tooltip: 'Stop/Ask',
+                    child: _isLoading || _isAsking
+                        ? const Icon(Icons.stop)
+                        : const Icon(Icons.play_arrow),
                   ),
                 ),
               ],
@@ -145,19 +160,13 @@ class _ApiIntegrationWidgetState extends ConsumerState<ApiIntegrationWidget>
                     foregroundColor: Theme.of(context).colorScheme.background,
                     backgroundColor: Theme.of(context).colorScheme.primary,
                   ),
-                  onPressed: _isLoading ? null : () {
-                    if (_imageBytes != null) {
-                      _generateTextWithImage();
-                    } else {
-                      _generateText();
-                    }
-                  },
-                  child: _isLoading
-                      ? SizedBox(
-                          width: 20.0, // Adjust the width as needed
-                          height: 20.0, // Adjust the height as needed
+                  onPressed: _isLoading || _isAsking ? null : _ask,
+                  child: _isLoading || _isAsking
+                      ? const SizedBox(
+                          width: 20.0,
+                          height: 20.0,
                           child: CircularProgressIndicator(
-                            strokeWidth: 2.0, // Decrease the stroke width
+                            strokeWidth: 2.0,
                           ),
                         )
                       : const Text('Ask'),
@@ -170,30 +179,66 @@ class _ApiIntegrationWidgetState extends ConsumerState<ApiIntegrationWidget>
     );
   }
 
-  Future<void> _generateText() async {
+  Future<void> _ask() async {
     final text = _promptInputController.text;
-    if (text.isEmpty) {
-      return;
+    if (_imageBytes != null) {
+      await _generateTextWithImage();
+    } else {
+      await _generateText();
     }
+  }
 
+  Future<void> _generateText() async {
     setState(() {
       _errorMessage = null;
-      _isLoading = true; // Set loading state to true
+      _isLoading = true;
+      mdText = '';
     });
 
     try {
       final gemini = Gemini.instance;
-      final result = await gemini.text(text);
+      final result = await gemini.text(_promptInputController.text);
       setState(() {
         mdText = result?.output ?? 'No output';
-        _isLoading = false; // Set loading state to false after completion
+        _isLoading = false;
       });
     } catch (e) {
       setState(() {
         _errorMessage = 'Error generating text: $e';
-        _isLoading = false; // Set loading state to false on error
+        _isLoading = false;
       });
     }
+  }
+
+  Future<void> _generateTextWithImage() async {
+    setState(() {
+      _errorMessage = null;
+      _isLoading = true;
+      mdText = '';
+    });
+
+    try {
+      final gemini = Gemini.instance;
+      final result = await gemini.textAndImage(
+        text: _promptInputController.text,
+        images: [_imageBytes!],
+      );
+      setState(() {
+        mdText = result?.content?.parts?.last.text ?? 'No output';
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _errorMessage = 'Error generating text with image: $e';
+        _isLoading = false;
+      });
+    }
+  }
+
+  void _stopAsking() {
+    setState(() {
+      _isAsking = false;
+    });
   }
 
   Future<void> _pickImage() async {
@@ -213,38 +258,10 @@ class _ApiIntegrationWidgetState extends ConsumerState<ApiIntegrationWidget>
     });
   }
 
-  Future<void> _generateTextWithImage() async {
-    if (_imageBytes == null) {
-      return;
-    }
-
-    setState(() {
-      _errorMessage = null;
-      _isLoading = true; // Set loading state to true
-    });
-
-    try {
-      final gemini = Gemini.instance;
-      final result = await gemini.textAndImage(
-        text: _promptInputController.text,
-        images: [_imageBytes!],
-      );
-      setState(() {
-        mdText = result?.content?.parts?.last.text ?? 'No output';
-        _isLoading = false; // Set loading state to false after completion
-      });
-    } catch (e) {
-      setState(() {
-        _errorMessage = 'Error generating text with image: $e';
-        _isLoading = false; // Set loading state to false on error
-      });
-    }
-  }
-
   Future<void> _copyText() async {
     await Clipboard.setData(ClipboardData(text: mdText));
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Text copied to clipboard')),
+      const SnackBar(content: Text('Text copied to clipboard')),
     );
   }
 

lib/Routes/app_route_config.dart
@@ -2,7 +2,6 @@ import 'package:ChatGemini/Routes/app_route_constants.dart';
 import 'package:ChatGemini/screens/Authentication/auth_page.dart';
 import 'package:ChatGemini/screens/Home/home_page.dart';
 import 'package:ChatGemini/screens/Profile/profile.dart';
-import 'package:ChatGemini/screens/Safety/safety.dart';
 import 'package:go_router/go_router.dart';
 
 class MyAppRouter {
@@ -28,12 +27,12 @@ class MyAppRouter {
         return const Profile();
       },
     ),
-    GoRoute(
-      name: MyAppRoutConstants.safetyRoute,
-      path: '/Safety',
-      builder: (context, state) {
-        return const Safety();
-      },
-    ),
+    // GoRoute(
+    //   name: MyAppRoutConstants.safetyRoute,
+    //   path: '/Safety',
+    //   builder: (context, state) {
+    //     return const Safety();
+    //   },
+    // ),
   ]);
 }

lib/screens/Home/home_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:ChatGemini/globals.dart';
-import 'package:ChatGemini/screens/Safety/safety.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:ChatGemini/screens/Profile/profile.dart';
@@ -100,7 +99,7 @@ class _HomePageState extends State<HomePage> {
             const Profile(),
             // const SettingsPage(),
             //TODO: Update settings to also show only if platform is android.
-            const Safety()
+            // const Safety()
           ],
           onPageChanged: (index) {
             setState(() {
@@ -139,11 +138,11 @@ class _HomePageState extends State<HomePage> {
             //     label: "Settings",
             //     backgroundColor: Colors.black,
             //   ),
-              BottomNavigationBarItem(
-              icon: Icon(Icons.health_and_safety),
-              label: "Safety",
-              backgroundColor: Colors.black,
-            ),
+            //   BottomNavigationBarItem(
+            //   icon: Icon(Icons.health_and_safety),
+            //   label: "Safety",
+            //   backgroundColor: Colors.black,
+            // ),
           ],
         ),
       ),

lib/screens/Safety/safety.dart
@@ -1,233 +1,0 @@
-import 'package:ChatGemini/env/env.dart';
-import 'package:ChatGemini/providers/parameter_provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:ChatGemini/globals.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// API Key from Envied .env file
-String apiKey = Env.geminiApiKey;
-
-// double _temperature = 0.0;
-double _temperature = global_temperature;
-
-// API Key from globals.dart
-// String apiKey = PALM_API_KEY;
-
-final _obscureTextNotifier = ValueNotifier<bool>(true);
-final TextEditingController _apiTextController = TextEditingController();
-final TextEditingController _nameTextController = TextEditingController();
-
-class Safety extends ConsumerStatefulWidget {
-  const Safety({super.key});
-
-  @override
-  _SafetyState createState() => _SafetyState();
-}
-
-class _SafetyState extends ConsumerState<Safety> {
-  bool _obscureText = true;
-
-  Future<void> _saveData() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setDouble('temperature', ref.read(temperatureProvider));
-    await prefs.setDouble('topK', ref.read(topKProvider));
-    await prefs.setDouble('topP', ref.read(topPProvider));
-  }
-
-  void signUserOut() async {
-    await FirebaseAuth.instance.signOut();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _loadUserName();
-    _initializeParameters();
-  }
-
-  void _initializeParameters() async {
-    final prefs = await SharedPreferences.getInstance();
-    final temperature = prefs.getDouble('temperature') ?? 0.7;
-    final topK = prefs.getDouble('topK') ?? 40.0;
-    final topP = prefs.getDouble('topP') ?? 0.95;
-    ref.read(temperatureProvider.notifier).state = temperature;
-    ref.read(topKProvider.notifier).state = topK;
-    ref.read(topPProvider.notifier).state = topP;
-  }
-
-  void _loadUserName() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      USER_NAME_GLOBAL = prefs.getString('userName') ?? '';
-    });
-  }
-
-  void _saveName() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('userName', USER_NAME_GLOBAL);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var temperature = ref.watch(temperatureProvider);
-    var topK = ref.watch(topKProvider);
-    var topP = ref.watch(topPProvider);
-
-    final isDarkMode =
-        MediaQuery.of(context).platformBrightness == Brightness.dark;
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // Define the primary color and text themes for dark mode
-        brightness: isDarkMode ? Brightness.dark : Brightness.light,
-        primarySwatch: Colors.teal,
-        textTheme: TextTheme(
-          titleLarge:
-              TextStyle(color: isDarkMode ? Colors.white : Colors.black),
-        ),
-      ),
-      home: Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Text(
-                  "Safety",
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Divider(
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Temperature",
-                      style: GoogleFonts.inter(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                    ),
-                    Text(
-                      temperature.toStringAsFixed(1),
-                      style: GoogleFonts.inter(
-                        color: Theme.of(context).colorScheme.primary,
-                        fontSize: 18,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 175,
-                      child: Slider(
-                          value: temperature,
-                          min: 0,
-                          max: 1,
-                          divisions: 10,
-                          onChanged: (value) {
-                            ref.read(temperatureProvider.notifier).state =
-                                value;
-                            _saveData();
-                          }),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "topK",
-                      style: GoogleFonts.inter(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                    ),
-                    Text(
-                      topK.toStringAsFixed(1),
-                      style: GoogleFonts.inter(
-                        color: Theme.of(context).colorScheme.primary,
-                        fontSize: 18,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 175,
-                      child: Slider(
-                          value: topK,
-                          min: 0,
-                          max: 100,
-                          divisions: 10,
-                          onChanged: (value) {
-                            ref.read(topKProvider.notifier).state = value;
-                            _saveData();
-                          }),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "topP",
-                      style: GoogleFonts.inter(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                    ),
-                    Text(
-                      topP.toStringAsFixed(1),
-                      style: GoogleFonts.inter(
-                        color: Theme.of(context).colorScheme.primary,
-                        fontSize: 18,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 175,
-                      child: Slider(
-                          value: topP,
-                          min: 0,
-                          max: 1,
-                          divisions: 10,
-                          onChanged: (value) {
-                            ref.read(topPProvider.notifier).state = value;
-                            _saveData();
-                          }),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}

commit c307c5aa0da558d0f46b3a895b31f0dacbc39c84

Fix: Stream Text Broken after PR #14 (#15)
* Added type cursor

* Added an autoscroller

* Added stop button and removed Safety

* Fixed Project Warnings

* Update api_integration_widget.dart

* Update api_integration_widget.dart

---------

Co-authored-by: Vedant Heda <vedant.heda@outlook.com>
Co-authored-by: Vedant Heda <90914768+vedantheda@users.noreply.github.com>

lib/widgets/api_integration_widget.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
@@ -22,12 +24,12 @@ class _ApiIntegrationWidgetState extends State<ApiIntegrationWidget>
   Uint8List? _imageBytes;
   String? _errorMessage;
   bool _isLoading = false;
-  late bool _isAsking;
+  bool _isAsking = false;
+  late Timer _typingTimer;
 
   @override
   void initState() {
     super.initState();
-    _isAsking = false;
   }
 
   @override
@@ -103,7 +105,8 @@ class _ApiIntegrationWidgetState extends State<ApiIntegrationWidget>
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: FloatingActionButton(
-                    onPressed: _isLoading ? null : _copyText,
+                    onPressed:
+                        _isLoading ? null : _copyText,
                     tooltip: 'Copy Text',
                     child: const Icon(Icons.copy),
                   ),
@@ -112,7 +115,7 @@ class _ApiIntegrationWidgetState extends State<ApiIntegrationWidget>
                   padding: const EdgeInsets.all(8.0),
                   child: FloatingActionButton(
                     onPressed:
-                        _isAsking ? _stopAsking : (_isLoading ? null : _ask),
+                        _isLoading ? _stopAsking : (_isAsking ? _stopAsking : null),
                     tooltip: 'Stop/Ask',
                     child: _isLoading || _isAsking
                         ? const Icon(Icons.stop)
@@ -162,7 +165,7 @@ class _ApiIntegrationWidgetState extends State<ApiIntegrationWidget>
                   ),
                   onPressed: _isLoading || _isAsking ? null : _ask,
                   child: _isLoading || _isAsking
-                      ? const SizedBox(
+                      ? SizedBox(
                           width: 20.0,
                           height: 20.0,
                           child: CircularProgressIndicator(
@@ -198,10 +201,7 @@ class _ApiIntegrationWidgetState extends State<ApiIntegrationWidget>
     try {
       final gemini = Gemini.instance;
       final result = await gemini.text(_promptInputController.text);
-      setState(() {
-        mdText = result?.output ?? 'No output';
-        _isLoading = false;
-      });
+      _startTyping(result?.output ?? 'No output');
     } catch (e) {
       setState(() {
         _errorMessage = 'Error generating text: $e';
@@ -223,10 +223,7 @@ class _ApiIntegrationWidgetState extends State<ApiIntegrationWidget>
         text: _promptInputController.text,
         images: [_imageBytes!],
       );
-      setState(() {
-        mdText = result?.content?.parts?.last.text ?? 'No output';
-        _isLoading = false;
-      });
+      _startTyping(result?.content?.parts?.last.text ?? 'No output');
     } catch (e) {
       setState(() {
         _errorMessage = 'Error generating text with image: $e';
@@ -235,8 +232,27 @@ class _ApiIntegrationWidgetState extends State<ApiIntegrationWidget>
     }
   }
 
+  void _startTyping(String text) {
+    const typingInterval = Duration(milliseconds: 15);
+    int i = 0;
+
+    _typingTimer = Timer.periodic(typingInterval, (timer) {
+      setState(() {
+        if (i < text.length) {
+          mdText = text.substring(0, i + 1);
+          i++;
+        } else {
+          timer.cancel();
+          _isLoading = false;
+        }
+      });
+    });
+  }
+
   void _stopAsking() {
+    _typingTimer.cancel(); // Cancel the timer when stopping asking
     setState(() {
+      _isLoading = false; // Update isLoading state to false
       _isAsking = false;
     });
   }
@@ -251,7 +267,6 @@ class _ApiIntegrationWidgetState extends State<ApiIntegrationWidget>
       });
     }
   }
-
   void _removeImage() {
     setState(() {
       _imageBytes = null;