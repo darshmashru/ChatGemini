ec2f41b69fa2137513161f5040919819bbd52b97
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2024-03-03 18:14:04 +0530

    Fixed Google Sign in on Web (#19)

--- lib/screens/Authentication/login_page.dart
+++ lib/screens/Authentication/login_page.dart
@@ -6,6 +6,7 @@ import "package:ChatGemini/screens/Home/home_page.dart";
 import "package:ChatGemini/services/auth_service.dart";
 import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/material.dart";
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class LoginPage extends StatefulWidget {
   final Function? onTap;
@@ -161,14 +162,8 @@ class _LoginPageState extends State<LoginPage> {
                   children: [
                     Squaretile(
                       imagePath: "lib/assets/images/google.png",
-                      onTap: () => AuthService().signInWithGoogle(),
+                      onTap: () => (AuthService().signInWithGoogle()),
                     ),
-                    // const SizedBox(width: 10),
-                    // Squaretile(
-                    //   // imagePath: "lib/assets/images/apple.png",
-                    //   imagePath: "lib/assets/images/Apple_logo_white.png",
-                    //   onTap: () {},
-                    // )
                   ],
                 ),
                 const SizedBox(height: 30),
